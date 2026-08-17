75eade368ddfe8e9f8ee7bde9cb747c49edaaf1a
Михаил Панков <mikhailpankov@elementpay.io>
2026-08-17 13:49:27 +0700

    fix: start API modules for catalog and auth

--- src/book-purchase/book-purchase.module.ts
+++ src/book-purchase/book-purchase.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BookModule } from '../book/book.module';
+import { AuthModule } from '../auth/auth.module';
 import { WalletModule } from '../wallet/wallet.module';
 import { BookPurchaseController } from './book-purchase.controller';
 import { BookPurchaseService } from './book-purchase.service';
 
 @Module({
-  imports: [BookModule, WalletModule],
+  imports: [BookModule, AuthModule, WalletModule],
   controllers: [BookPurchaseController],
   providers: [BookPurchaseService],
+  exports: [BookPurchaseService],
 })
 export class BookPurchaseModule {}
